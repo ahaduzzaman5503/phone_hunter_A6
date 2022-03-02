c58301812e066043d0b6128f2d98474e96df6496
ahaduzzaman550 <ahaduzzaman5503@gmail.com>
2022-03-02 23:59:54 +0600

    d

--- api.js
+++ api.js
@@ -1,24 +1,18 @@
 const searchPhone = () => {
     const searchFeild = document.getElementById('input-feild');
     const searchValue = searchFeild.value;
-    // console.log(searchValue);
-    
     searchFeild.value = "";
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
-
-    // console.log(url);
     fetch(url)
     .then (res => res.json())
     .then (data => displayPhoneResult(data.data));
 }
 
 const displayPhoneResult = iPhone => {
-    // console.log(iPhone);
     const searchRasult = document.getElementById('class-rasult');
-    // console.log(searchRasult);
+    searchRasult.innerHTML = "";
     iPhone.forEach(phone => {
-        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add("col");
         div.innerHTML= `
@@ -27,10 +21,44 @@ const displayPhoneResult = iPhone => {
             <div class="card-body">
               <h5 class="card-title">${phone.brand}</h5>
               <h5 class="card-title">${phone.phone_name}</h5>
-              <a href="#" class="btn btn-primary">Details</a>
+              <a href="#" onclick="showPhoneDetails()"  class="btn btn-primary">Details</a>
             </div>
           </div>
         `;
         searchRasult.appendChild(div);
     });
 }
+
+const searchFeild = document.getElementById('input-feild');
+    const searchValue = searchFeild.value;
+
+    const searchButton = () => {
+      const searchFeild = document.getElementById('input-feild');
+      const searchValue = searchFeild.value;
+      searchFeild.value = "";
+  
+      const urlOne = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`
+      fetch(urlOne)
+      .then (res => res.json())
+      .then (data => showPhoneDetails(data.data));
+  }
+  const showPhoneDetails =  (displayPhoneResult, showData ) => {
+  const phoneDetails = document.getElementById('details');
+  // showData.forEach(show=> {
+    const p = document.createElement('p');
+    p.classList.add("col");
+    p.innerHTML= `
+          <div class="card h-100 ">
+              <img src="https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Phone Brand:...</h5>
+                <h5 class="card-title">Phone Name:...</h5>
+                <h5 class="card-title">Phone :...</h5>
+              </div>
+            </div>
+          `;
+    phoneDetails.appendChild(p);
+  console.log("click")
+  // })
+  
+};
